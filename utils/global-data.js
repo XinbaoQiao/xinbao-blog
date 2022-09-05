18b8d697c5e0f6782e72fdf80d0dcfd78d607919
南有乔木 <1138687504@qq.com>
2022-09-06 01:16:33 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '南有乔木';
+    : 'Xinbao';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Welcome to my blog';
